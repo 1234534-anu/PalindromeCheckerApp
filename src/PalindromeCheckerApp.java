f46813ac1eaa5f80b4a6cd612a2bc170e84574f1
1234534-anu <anujamurugan0@gmail.com>
2026-02-25 10:46:21 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker");
+        System.out.println("Version 1.0");
+
+    }
+}
